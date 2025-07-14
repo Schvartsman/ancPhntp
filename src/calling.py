e818bdc4dd38ba1162aed0614dc665c0f2f852ad
Daniil <shvartsman.dr@phystech.edu>
2025-07-14 15:08:41 +0300

    Distingtion of strings and split one of types

--- src/calling.py
+++ src/calling.py
@@ -8,9 +8,20 @@ def argument():
                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--sam", type=str,
                         help="Input sam-file for calling")
+    parser.add_argument("--ref", type=str,
+                        help="Number of reference genome (19 or 38), which was applied in alignment")
     args=parser.parse_args()
     return args
 
 args = argument()
 sam = args.sam
-print(sam)
+ref = args.ref
+f = open(sam, 'r')
+for s in f:
+    if s[0] == '@':
+        continue
+    else:
+        x = s.split('\t')
+        break
+print(x)
+f.close()
